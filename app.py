84f4b17d682bd1dd0d611dd93d586c4af36f5507
Imaaneea <imaane.elayachi@gmail.com>
2024-12-29 20:11:54 +0100

    app.py

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ import pickle
 import pandas as pd
 
 # Charger le pipeline sauvegardé
-<<<<<<< HEAD
+
 pipeline = pickle.load(open("car_price_rf.pkl", mode="rb"))
-=======
-pipeline = pickle.load(open("car_price_gbr.pkl", mode="rb"))
->>>>>>> 54de93099c6ad0363d5c0d46bb578526fe7c9fcd
+
 
 # Titre de l'application
 st.title("Prédiction du Prix des Voitures 🚗")
